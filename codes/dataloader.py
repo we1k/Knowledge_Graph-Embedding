dce9ed8c5e6729e4060aa7c77bcc00f01acfdbf5
we1k <43416079+we1k@users.noreply.github.com>
2020-12-09 22:43:22 +0800

    dataloader

--- codes/dataloader.py
+++ codes/dataloader.py
@@ -79,6 +79,7 @@ class TrainDataset(Dataset):
                         walker = neighbors.indices[idx]
                         neighbors = a_mat[walker]
                     k_mat[i, walker] += 1
+
         logging.info(f'randomly_sampled: {randomly_sampled}')
         k_mat = k_mat.tocsr()
         
@@ -112,7 +113,63 @@ class TrainDataset(Dataset):
         # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
         # 2. Preprocess the data (e.g. torchvision.Transform).
         # 3. Return a data pair (e.g. image and label).
-        pass
+        positive_sample = self.triples[idx]
+        head, relation, tail = positive_sample
+
+        subsample_weight = self.count[(head, relation)] + self.count[(tail, -relation - 1)]
+        subsample_weight = torch.Tensor(1 / torch.Tensor([subsample_weight]))
+        
+        # indice of negative sample
+        negative_sample_list = []
+        negative_sample_size = 0
+        
+        k_hop_flag = True
+
+        while negative_sample_size < self.negative_sample_size:
+            if self.k_neighbors is not None and k_hop_flag:
+                if self.mode == 'head-batch':
+                    khop = self.k_neighbors[tail].indices
+                elif self.mode == 'tail-batch':
+                    khop = self.k_neighbors[head].indices
+                else:
+                    raise ValueError(f'Training batch mode {self.mode} not supported')   
+                negative_sample = khop[np.random.randint(len(khop), size=self.negative_sample_size * 2)].astype(
+                    np.int64)
+            else:
+                negative_sample = np.random.randint(self.nentity, size=self.negative_sample_size * 2)
+            
+            # guaranteen the observed true sample not been chosen
+            if self.mode == 'head-batch':
+                mask = np.in1d(
+                    negative_sample,
+                    self.true_head[(relation, tail)],
+                    assume_unique=True,
+                    invert=True
+                )
+            elif self.mode == 'tail-batch':
+                mask = np.in1d(
+                    negative_sample,
+                    self.true_tail[(head, relation)],
+                    assume_unique=True,
+                    invert=True
+                )
+            else:
+                raise ValueError(f'Traing batch mode {self.mode} not supported')
+            
+            negative_sample = negative_sample[mask]
+            negative_sample_list.append(negative_sample)
+            
+            if negative_sample.size == 0:
+                k_hop_flag = False
+            negative_sample_size += negative_sample.size
+        
+        negative_sample = np.concatenate(negative_sample_list)[:self.negative_sample_size]
+
+        negative_sample = torch.from_numpy(negative_sample)
+
+        positive_sample = torch.LongTensor(positive_sample)
+
+        return postive_sample, negative_sample, subsample_weight, self.mode
 
     @staticmethod
     def collate_fn(data):
@@ -146,7 +203,71 @@ class TrainDataset(Dataset):
 
     @staticmethod
     def get_true_head_and_tail(triples):
-        pass
+        '''
+        Build a dictionary of true triples that will
+        be used to filter these true triples for negative sampling
+        '''
+        true_head = {}
+        true_tail = {}
+
+        for head, relation, tail in triples:
+            if (head, relation) not in true_tail:
+                true_tail[(head, relation)] = []
+            true_tail[(head, relation)].append(tail)
+            if (relation, tail) not in true_head:
+                true_head[relation, tail)] = []
+            true_head[(relation, tail)].append(head)
+        
+        # unique the idx of true_head and tail
+        for relation, tail in true_head:
+            true_head[(relation, tail)] = np.array(list(set(true_head[(relation, tail)])))
+        for head, relation in true_tail:
+            true_tail[(head, relation)] = np.array(list(set(true_tail[(head, relation)])))
+
+        return true_head, true_tail
+
+class TestDataset(Dataset):
+    def __init__(self, triples, all_true_triples, nentity, nrelation, mode):
+        self.len = len(triples)
+        self.triple_set = set(all_true_triples)
+        self.triples = triples
+        self.nentity = nentity
+        self.nrelation = nrelation
+        self.mode = mode
+    
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        head, relation, tail = self.triples[idx]
+
+        if self.mode == 'head-batch':
+            tmp = [(0, rand_head) if (rand_head, relation, tail) not in self.triple_set
+                   else (-1, head) for rand_head in range(self.nentity)]
+            tmp[head] = (0, head)
+        elif self.mode == 'tail-batch':
+            tmp = [(0, rand_tail) if (head, relation, rand_tail) not in self.triple_set
+                   else (-1, tail) for rand_tail in range(self.nentity)]
+            tmp[tail] = (0, tail)
+        else:
+            raise ValueError('negative batch mode %s not supported' % self.mode)
+
+        tmp = torch.LongTensor(tmp)
+        filter_bias = tmp[:, 0].float()
+        negative_sample = tmp[:, 1]
+
+        positive_sample = torch.LongTensor((head, relation, tail))
+
+        return positive_sample, negative_sample, filter_bias, self.mode
+        
+    @staticmethod
+    def collate_fn(data):
+        positive_sample = torch.stack([_[0] for _ in data], dim=0)
+        negative_sample = torch.stack([_[1] for _ in data], dim=0)
+        filter_bias = torch.stack([_[2] for _ in data], dim=0)
+        mode = data[0][3]
+        return positive_sample, negative_sample, filter_bias, mode
+
 
 # todo
 class BidirectionalOneShotIterator(object):
