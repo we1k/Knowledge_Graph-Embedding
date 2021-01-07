6314e5cf98825dbeaa373bbec0e223333dc0862f
we1k <43416079+we1k@users.noreply.github.com>
2021-01-07 17:03:03 +0800

    1

---
+++ codes/run.py
@@ -0,0 +1,337 @@
+import argparse
+import json
+import logging
+import os
+import random
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from model import KGEModel
+from dataloader import TrainDataset
+from dataloader import BidirectionalOneShotIterator
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Training and Testing Knowledge Graph Embedding Models',
+        usage='train.py [<args>] [-h | --help]'
+    )
+
+    parser.add_argument('--cuda', action='store_true', help='use GPU')
+
+    parser.add_argument('--do_train', action='store_true')
+    parser.add_argument('--do_valid', action='store_true')
+    parser.add_argument('--do_test', action='store_true')
+    parser.add_argument('--evaluate_train', action='store_true', help='Evaluate on training data')
+
+    parser.add_argument('--data_path', type=str, default=None)
+    parser.add_argument('--model', default='TransE', type=str)
+    parser.add_argument('-de', '--double_entity_embedding', action='store_true')
+    parser.add_argument('-dr', '--double_relation_embedding', action='store_true')
+
+    parser.add_argument('-n', '--negative_sample_size', default=128, type=int)
+    parser.add_argument('-d', '--hidden_dim', default=500, type=int)
+    parser.add_argument('-g', '--gamma', default=12.0, type=float)
+    parser.add_argument('-adv', '--negative_adversarial_sampling', action='store_true')
+    parser.add_argument('-a', '--adversarial_temperature', default=1.0, type=float)
+    parser.add_argument('-khop', '--negative_k_hop_sampling', default=0, type=int)
+    parser.add_argument('-nrw', '--negative_n_random_walks', default=0, type=int)
+    parser.add_argument('-b', '--batch_size', default=1024, type=int)
+    parser.add_argument('-r', '--regularization', default=0.0, type=float)
+    parser.add_argument('--test_batch_size', default=4, type=int, help='valid/test batch size')
+    parser.add_argument('--uni_weight', action='store_true',
+                        help='Otherwise use subsampling weighting like in word2vec')
+
+    parser.add_argument('-lr', '--learning_rate', default=0.0001, type=float)
+    parser.add_argument('-cpu', '--cpu_num', default=10, type=int)
+    parser.add_argument('-init', '--init_checkpoint', default=None, type=str)
+    parser.add_argument('-save', '--save_path', default=None, type=str)
+    parser.add_argument('--max_steps', default=100000, type=int)
+    parser.add_argument('--warm_up_steps', default=None, type=int)
+
+    parser.add_argument('--save_checkpoint_steps', default=10000, type=int)
+    parser.add_argument('--valid_steps', default=10000, type=int)
+    parser.add_argument('--log_steps', default=100, type=int, help='train log every xx steps')
+    parser.add_argument('--test_log_steps', default=1000, type=int, help='valid/test log every xx steps')
+
+    parser.add_argument('--nentity', type=int, default=0, help='DO NOT MANUALLY SET')
+    parser.add_argument('--nrelation', type=int, default=0, help='DO NOT MANUALLY SET')
+
+    return parser.parse_args(args)
+
+
+def override_config(args):
+    '''
+    Override model and data configuration
+    '''
+    pass
+
+def save_model(model, optimizer, save_variable_list, args):
+    '''
+    Save the parameters of the model and the optimizer,
+    as well as some other variables such as step and learning_rate
+    '''
+    pass
+
+def read_triple(file_path, entity2id, relation2id):
+    triples = []
+    with open(file_path) as f:
+        for line in f:
+            h, r, t = line.strip().split('\t')
+            triples.append((entity2id[h], relation2id[r], entity2id[t]))
+    return triples
+
+def set_logger(args):
+    '''
+    Write logs to checkpoint and console
+    '''
+
+    if args.do_train:
+        log_file = os.path.join(arg.save_path or args.init_checkpoint, 'train.log')
+    else:
+        log_file = os.path.join(arg.save_path or args.init_checkpoint, 'test.log')
+    
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename=log_file,
+        filemode='w'
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+def log_metrics(mode, step, metrics):
+    for metric in metrics:
+        logging.info('%s %s at step %d: %f' % (mode, metric, step, metrics[metric]))
+
+
+def main(args):
+    if (not args.do_train) and (not args.do_valid) and (not args.do_test):
+        raise ValueError('one of train/val/test mode must be chosen')
+    
+    if args.init_checkpoint:
+        override_config(args)
+    elif args.data_path is None:
+        raise ValueError('one of init_checkpoint/data_path must be chosen')
+    
+    if args.do_train and args.save_path is None:
+        raise ValueError('Where do you want to save your trained model?')
+
+    if args.save_path and not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    # set logs to console and checkpoint
+    set_logger(args)
+
+    with open(os.path.join(args.data_path, 'entities.dict')) as fin:
+        entity2id = dict()
+        for line in fin:
+            eid, entity = line.strip().split('\t')
+            entity2id[entity] = int(eid)
+
+    with open(os.path.join(args.data_path, 'relations.dict')) as fin:
+        relation2id = dict()
+        for line in fin:
+            rid, relation = line.strip().split('\t')
+            relation2id[relation] = int(rid)
+    
+    nentity = len(entity2id)
+    nrelation = len(relation2id)
+    
+    args.nentity = nentity
+    args.nrelation = nrelation
+
+    logging.info('Model: %s' % args.model)
+    logging.info('Data Path: %s' % args.data_path)
+    logging.info('#entity: %d' % nentity)
+    logging.info('#relation: %d' % nrelation)
+
+    # test should change to test.txt
+    train_triples = read_triple(os.path.join(args.data_path, 'train.txt'), entity2id, relation2id)
+    logging.info('#train: %d' % len(train_triples))
+    valid_triples = read_triple(os.path.join(args.data_path, 'valid.txt'), entity2id, relation2id)
+    logging.info('#valid: %d' % len(valid_triples))
+    test_triples = read_triple(os.path.join(args.data_path, 'test.txt'), entity2id, relation2id)
+    logging.info('#test: %d' % len(test_triples))
+
+    #test
+    # for triple in test_triples:
+    #   print(triple[0], " ", triple[1], " ", triple[2])
+    all_true_triples = train_triples + valid_triples + test_triples
+
+    kge_model = KGEModel(
+        model_name=args.model,
+        nentity=nentity,
+        nrelation=nrelation,
+        hidden_dim=args.hidden_dim,
+        gamma=args.gamma,
+        double_entity_embedding=args.double_entity_embedding,
+        double_relation_embedding=args.double_relation_embedding
+    )
+
+    logging.info('Model Parameter Configuration:')
+    for name, param in kge_model.named_parameters():
+        logging.info('Parameter %s: %s, require_grad = %s' % (name, str(param.size()), str(param.requires_grad)))
+
+    if args.cuda:
+        kge_model = kge_model.cuda()
+
+    if args.do_train:
+        train_dataloader_head = DataLoader(
+            TrainDataset(train_triples,
+                         nentity,
+                         nrelation,
+                         args.negative_sample_size,
+                         'head-batch',
+                         args.negative_k_hop_sampling,
+                         args.negative_n_random_walks,
+                         dsn=args.data_path),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=max(1. args.cpu_num//2),
+            collate_fn=TrainDataset.collate_fn
+        )
+
+        train_dataloader_tail = DataLoader(
+            TrainDataset(train_triples,
+                         nentity,
+                         nrelation,
+                         args.negative_sample_size,
+                         'tail-batch',
+                         args.negative_k_hop_sampling,
+                         args.negative_n_random_walks,
+                         dsn=args.data_path),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=max(1, args.cpu_num//2),
+            collate_fn=TrainDataset.collate_fn
+        )
+
+        train_iterator = BidirectionalOneShotIterator(train_dataloader_head, train_dataloader_tail)
+
+        # Set training configuration
+        current_learning_rate = args.learning_rate
+
+        optimizer = torch.optim.Adam(
+            filter(lambda p: p.require_grad, kge_model.parameters()),
+            lr=current_learning_rate
+        )
+
+        if args.warm_up_steps:
+            warm_up_steps = args.warm_up_steps
+        else:
+            warm_up_steps = args.max_steps // 2
+            
+    if args.init_checkpoint:
+        # Restore model from checkpoint directory
+        logging.info('Loading checkpoint %s...' % args.init_checkpoint)
+        checkpoint = torch.load(os.path.join(args.init_checkpoint, 'checkpoint'))
+        init_step = checkpoint['step']
+        kge_model.load_state_dict(checkpoint['model_state_dict'])
+        if args.do_train:
+            current_learning_rate = checkpoint['current_learning_rate']
+            warm_up_steps = checkpoint['warm_up_steps']
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    else:
+        logging.info('Ramdomly Initializing %s Model...' % args.model)
+        init_step = 0
+
+    step = init_step
+
+    logging.info('Start Training...')
+    logging.info('init_step = %d' % init_step)
+    logging.info('batch_size = %d' % args.batch_size)
+    logging.info('negative_adversarial_sampling = %d' % args.negative_adversarial_sampling)
+    logging.info('negative_k_hop_sampling = %d' % args.negative_k_hop_sampling)
+    logging.info('negative_n_random_walks = %d' % args.negative_n_random_walks)
+    logging.info('hidden_dim = %d' % args.hidden_dim)
+    logging.info('gamma = %f' % args.gamma)
+    logging.info('negative_adversarial_sampling = %s' % str(args.negative_adversarial_sampling))
+    if args.negative_adversarial_sampling:
+        logging.info('adversarial_temperature = %f' % args.adversarial_temperature)
+
+    # Set valid dataloader as it would be evaluated during training
+
+    if args.do_train:
+        logging.info('learning_rate = %d' % current_learning_rate)
+
+        training_logs = []
+
+        # training Loop
+        for step in range(init_step, args.max_steps):
+            
+            log = kge_model.train_step(kge_model, optimizer, train_iterator, args)
+
+            training_logs.append(log)
+
+            if step >= warm_up_steps:
+                current_learning_rate = current_learning_rate / 10
+                logging.info('Change learning_rate to %f at step %d' % (current_learning_rate, step))
+                optimizer = torch.optim.Adam(
+                    filter(lambda p: p.require_grad, kge_model.parameters()),
+                    lr=current_learning_rate
+                )
+                warm_up_steps = warm_up_steps * 3
+
+            if step % args.save_checkpoint_steps == 0:
+                                save_variable_list = {
+                    'step': step,
+                    'current_learning_rate': current_learning_rate,
+                    'warm_up_steps': warm_up_steps
+                }
+                save_model(kge_model, optimizer, save_variable_list, args)
+
+            if step % args.log_steps == 0:
+                metrics = {}
+                for metric in training_logs[0].keys():
+                    metrics[metric] = sum([log[metric] for log in training_logs])/len(training_logs)
+                log_metrics('Training average', step, metrics)
+                training_logs = []
+            
+            if args.do_valid and step % args.valid_steps == 0:
+                logging.info('Evaluating on Valid Dataset...')
+                metrics = kge_model.test_step(kge_model, valid_triples, all_true_triples, args)
+                log_metrics('Valid', step, metrics)
+
+        save_variable_list = {
+            'step': step,
+            'current_learning_rate': current_learning_rate,
+            'warm_up_steps': warm_up_steps
+        }
+        save_model(kge_model, optimizer, save_variable_list, args)
+    
+    if args.do_valid:
+        logging.info('Evaluating on Valid Dataset...')
+        metrics = kge_model.test_step(kge_model, valid_triples, all_true_triples, args)
+        log_metrics('Valid', step, metrics)
+
+    if args.do_test:
+        logging.info('Evaluating on Test Dataset...')
+        metrics = kge_model.test_step(kge_model, test_triples, all_true_triples, args)
+        log_metrics('Test', step, metrics)
+
+    if args.evaluate_train:
+        logging.info('Evaluating on Training Dataset...')
+        metrics = kge_model.test_step(kge_model, train_triples, all_true_triples, args)
+        log_metrics('Test', step, metrics)
+
+if __name__ == "__main__":
+    main(parse_args())
+    # li = [i for i in range(10)]
+    # def iter(list):
+    #     while True:
+    #         for i in list:
+    #             yield i
+    # build a generator
+    # dataloader = iter(li)
+    # for i in dataloader:
+    #     print(i)
+
+    # while True:
+    #     print(next(dataloader))
+    print(os.getcwd())
